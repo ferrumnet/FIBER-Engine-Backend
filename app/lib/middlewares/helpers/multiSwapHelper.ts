c24c27a6a4202616d0109c0042bf2268d42599e2
Zikriya <zikriya.mushtaq@ferrum.network>
2022-12-11 02:14:27 +0500

    Naming convention fixation

--- app/lib/middlewares/helpers/multiSwapHelper.ts
+++ app/lib/middlewares/helpers/multiSwapHelper.ts
@@ -24,7 +24,7 @@ module.exports = {
 
   getSwapSigned: async function (req: any) {
     let data: any = {};
-    data = await fiberEngine.SWAPForAbi(
+    data = await fiberEngine.swapForAbi(
         req.query.walletAddress,
         req.query.sourceTokenContractAddress, // goerli ada
         req.query.destinationTokenContractAddress, // bsc ada
@@ -37,7 +37,7 @@ module.exports = {
 
   getWithdrawSigned: async function (req: any) {
     let data: any = {};
-    data = await fiberEngine.SWAP(
+    data = await fiberEngine.withdraw(
       req.query.sourceTokenContractAddress, // goerli ada
       req.query.destinationTokenContractAddress, // bsc ada
       req.query.sourceNetworkChainId, // source chain id (goerli)
