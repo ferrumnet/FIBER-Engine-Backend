8bf9700ce566758a8b6bcb221ee69b995890520c
Zikriya <zikriya.mushtaq@ferrum.network>
2023-01-13 22:56:27 +0500

    cahche network with cron feature. getTokenCategorizedAndQuoteInformation api is fetching data for network from network chache

--- app/lib/middlewares/common.ts
+++ app/lib/middlewares/common.ts
@@ -2,6 +2,10 @@ import crypto from 'crypto';
 var CryptoJS = require("crypto-js");
 import * as jwt from 'jsonwebtoken';
 var fs = require("fs");
+const { ethers } = require("ethers");
+const routerAbiMainnet = require("../../../artifacts/contracts/common/uniswap/IUniswapV2Router02.sol/IUniswapV2Router02.json");
+const fundManagerAbiMainnet = require("../../../artifacts/contracts/upgradeable-Bridge/FundManager.sol/FundManager.json");
+const fiberRouterAbiMainnet = require("../../../artifacts/contracts/upgradeable-Bridge/FiberRouter.sol/FiberRouter.json");
 
 module.exports = {
   getHashedPassword: function (password: any) {
@@ -64,9 +68,86 @@ module.exports = {
     }
   },
 
+  getProvider: function (rpc: any) {
+    try {
+      return new ethers.providers.JsonRpcProvider(rpc);
+    } catch (e) {
+      console.log(e);
+      return null;
+    }
+  },
+
+  getDexContract: function (item: any) {
+    try {
+      let dexContract = new ethers.Contract(
+        item.router,
+        routerAbiMainnet.abi,
+        item.provider
+      );
+      return dexContract;
+    } catch (e) {
+      console.log(e);
+      return null;
+    }
+  },
+
+  getFundManagerContract: function (item: any) {
+    try {
+      let fundMangerContract = new ethers.Contract(
+        item.fundManager,
+        fundManagerAbiMainnet.abi,
+        item.provider
+      );
+      return fundMangerContract;
+    } catch (e) {
+      console.log(e);
+      return null;
+    }
+  },
+
+  getFiberRouterContract: function (item: any) {
+    try {
+      let fiberRouterContract = new ethers.Contract(
+        item.fiberRouter,
+        fiberRouterAbiMainnet.abi,
+        item.provider
+      );
+      return fiberRouterContract;
+    } catch (e) {
+      console.log(e);
+      return null;
+    }
+  },
+
+  convertIntoFIBERNetworks: async function (networks: any) {
+    try {
+      if (networks && networks.length) {
+        for (let index = 0;  index < networks.length; index++) {
+          let network = networks[index];
+          if(network){
+            console.log('network',network);
+            let multiswapNetworkFIBERInformation: any = {...network.multiswapNetworkFIBERInformation};
+            multiswapNetworkFIBERInformation.name =  network.name;
+            multiswapNetworkFIBERInformation.shortName =  network.networkShortName;
+            multiswapNetworkFIBERInformation.rpc =  multiswapNetworkFIBERInformation.rpcUrl;
+            multiswapNetworkFIBERInformation.chainId =  network.chainId;
+            multiswapNetworkFIBERInformation.provider = this.getProvider(multiswapNetworkFIBERInformation.rpc);
+            multiswapNetworkFIBERInformation.dexContract = this.getDexContract(multiswapNetworkFIBERInformation);
+            multiswapNetworkFIBERInformation.fundManagerContract = this.getFundManagerContract(multiswapNetworkFIBERInformation);
+            multiswapNetworkFIBERInformation.fiberRouterContract = this.getFiberRouterContract(multiswapNetworkFIBERInformation);
+            network.multiswapNetworkFIBERInformation = multiswapNetworkFIBERInformation;
+          }
+        }
+      }
+    } catch (e) {
+      console.log(e);
+    }
+    return networks;
+  },
+
   getNetworkByChainId: function (chainId: any) {
     try {
-      if ((global as any).networks) {
+      if ((global as any).networks && (global as any).networks.length) {
         return (global as any).networks.find((item: any) => item.chainId === chainId);
       }
     } catch (e) {
