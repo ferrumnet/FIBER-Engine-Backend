78da19d016a0bd290bf07d8845f80672d8605324
Zikriya <zikriya.mushtaq@ferrum.network>
2023-08-29 15:54:34 +0500

    exponential bug fixed and at withdraw and optimize the code for fiberNode related exponential

--- scripts/fiberNode.ts
+++ scripts/fiberNode.ts
@@ -130,7 +130,7 @@ module.exports = {
       const sourceFoundryTokenDecimal =
         await sourceFoundryTokenContract.decimals();
       let amount = (inputAmount * 10 ** Number(sourceTokenDecimal)).toString();
-      amount = this.convert(amount);
+      amount = (global as any).utils.convertFromExponentialToDecimal(amount);
       // is source token foundy asset
       const isFoundryAsset = await sourceFACCheck(
         sourceNetwork,
@@ -319,7 +319,9 @@ module.exports = {
       destinationAmountOut =
         (await sourceBridgeAmount) / recentCudosPriceInDollars;
       machineSourceBridgeAmount = destinationAmountOut * 10 ** 18;
-      machineSourceBridgeAmount = this.convert(machineSourceBridgeAmount);
+      machineSourceBridgeAmount = (
+        global as any
+      ).utils.convertFromExponentialToDecimal(machineSourceBridgeAmount);
       targetAssetType = "Foundry";
     }
 
@@ -337,29 +339,5 @@ module.exports = {
     data.destination.amount = String(destinationAmountOut);
     data.destination.bridgeAmount = machineSourceBridgeAmount;
     return data;
-  },
-
-  convert(n: any) {
-    var sign = +n < 0 ? "-" : "",
-      toStr = n.toString();
-    if (!/e/i.test(toStr)) {
-      return n;
-    }
-    var [lead, decimal, pow] = n
-      .toString()
-      .replace(/^-/, "")
-      .replace(/^([0-9]+)(e.*)/, "$1.$2")
-      .split(/e|\./);
-    return +pow < 0
-      ? sign +
-          "0." +
-          "0".repeat(Math.max(Math.abs(pow) - 1 || 0, 0)) +
-          lead +
-          decimal
-      : sign +
-          lead +
-          (+pow >= decimal.length
-            ? decimal + "0".repeat(Math.max(+pow - decimal.length || 0, 0))
-            : decimal.slice(0, +pow) + "." + decimal.slice(+pow));
   },
 };
