83f07acf5a77004847dc6f0b044db8c4eb6aac92
Zikriya <zikriya.mushtaq@ferrum.network>
2024-02-21 23:26:33 +0500

    Auth bug fixed

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -1,22 +1,20 @@
-var jwt = require('jsonwebtoken');
+var jwt = require("jsonwebtoken");
 
 module.exports = function () {
   return async function (req: any, res: any, next: any) {
-
     if (!req.headers.authorization) {
-      return res.http401('Authorization header missing');
+      return res.http401("Authorization header missing");
     } else {
       try {
-        const token = req.headers.authorization.split(' ')[1];
-        if (await authHelper.isTokenValid(token) == false) {
-          return res.http401('Invalid token');
+        const token = req.headers.authorization.split(" ")[1];
+        if ((await authHelper.isTokenValid(token)) == false) {
+          return res.http401("Invalid token");
         }
         next();
       } catch (error) {
         (global as any).log.error(error);
-        return res.http401('Invalid token');
+        return res.http401("Invalid token");
       }
     }
-
-  }
+  };
 };
