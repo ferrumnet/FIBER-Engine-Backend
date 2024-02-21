a21d0fdcd15791096037e76133e5f8449facbfc3
Zikriya <zikriya.mushtaq@ferrum.network>
2024-02-21 23:43:07 +0500

    Forge path changed

--- app/lib/middlewares/helpers/forgeHelpers/forgeContractHelper.ts
+++ app/lib/middlewares/helpers/forgeHelpers/forgeContractHelper.ts
@@ -5,7 +5,7 @@ import {
   WithdrawSignedAndSwapOneInch,
 } from "../../../../interfaces/forgeInterface";
 import { Swap, SwapOneInch } from "../../../../interfaces/fiberEngineInterface";
-const forgeAbi: any = require("../../../../../artifacts/contracts/upgradeable-Bridge/FiberRouter.sol/Forge.json");
+const forgeAbi: any = require("../../../../../Forge.json");
 const fiberRouterAbi: any = require("../../../../../artifacts/contracts/upgradeable-Bridge/FiberRouter.sol/FiberRouter.json");
 
 const web3 = (rpcUrl: any) => {
