4fd8873ad12414a3d8c7a76572f21019c3e6097b
Zikriya <zikriya.mushtaq@ferrum.network>
2024-02-28 16:39:56 +0500

    zero and less then zero case handled at getTokenCategorizedAndQuoteInformation api

--- app/lib/middlewares/helpers/stringHelper.ts
+++ app/lib/middlewares/helpers/stringHelper.ts
@@ -1,12 +1,1 @@
-// error messages
-let strErrorInvalidCredentials = 'Backend_Error_Invalid_Credentials'
-
-// success messages
-let strSuccess = 'Success'
-
-module.exports = {
-  strSuccess,
-  strErrorInvalidCredentials
- 
-}
-
+export const strErrorSwapInNotAvailable = "Swap is not available";
